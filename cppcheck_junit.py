de5546a0e4ecab1a6b3b035f8e0e07719b48eee1
johnthagen <johnthagen@gmail.com>
2016-03-22 17:50:51 -0400

    Fix type hint format.

--- cppcheck_junit.py
+++ cppcheck_junit.py
@@ -24,7 +24,7 @@ class CppcheckError(object):
                  error_id,  # type: str
                  verbose  # type: str
                  ):
-        # type: () -> CppcheckError
+        # type: (...) -> CppcheckError
         """Constructor.
 
         Args:
