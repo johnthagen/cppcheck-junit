51162951fe7d17b12655dd3c5bb180dbbaac2478
johnthagen <johnthagen@gmail.com>
2019-12-15 14:11:06 -0500

    Drop Python 3.4, add Python 3.8. Add license file

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
     author='John Hagen',
     author_email='johnthagen@gmail.com',
     url='https://github.com/johnthagen/cppcheck-junit',
-    license='MIT',
 
     py_modules=['cppcheck_junit'],
     install_requires=open('requirements.txt').readlines(),
     zip_safe=False,
 
+    license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
@@ -29,10 +29,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
