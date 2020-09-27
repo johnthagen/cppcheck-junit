559ec26e48871cd5dba8b1f2fa3b1a0754daa328
johnthagen <johnthagen@users.noreply.github.com>
2020-09-27 15:33:22 -0400

    Modernize linting (#11)
    * Modernize testing and linting and drop Python 3.5
    
    * Remove unnecessary license_files setup.py config

--- setup.py
+++ setup.py
@@ -1,50 +1,41 @@
 from setuptools import setup
 
-
 setup(
-    name='cppcheck-junit',
-    version='2.0.0',
-
-    description='Converts Cppcheck XML output to JUnit format.',
-    long_description=open('README.rst').read(),
-    keywords='Cppcheck C++ JUnit',
-
-    author='John Hagen',
-    author_email='johnthagen@gmail.com',
-    url='https://github.com/johnthagen/cppcheck-junit',
-
-    py_modules=['cppcheck_junit'],
-    install_requires=open('requirements.txt').readlines(),
-    python_requires='>=3.5',
+    name="cppcheck-junit",
+    version="2.0.0",
+    description="Converts Cppcheck XML output to JUnit format.",
+    long_description=open("README.rst").read(),
+    keywords="Cppcheck C++ JUnit",
+    author="John Hagen",
+    author_email="johnthagen@gmail.com",
+    url="https://github.com/johnthagen/cppcheck-junit",
+    py_modules=["cppcheck_junit"],
+    install_requires=open("requirements.txt").readlines(),
+    python_requires=">=3.6",
     zip_safe=False,
-
-    license='MIT',
-    license_files=['LICENSE.txt'],
+    license="MIT",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: C++',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Software Development :: Quality Assurance',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: C++",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
     ],
-
-    scripts=['cppcheck_junit.py'],
-
+    scripts=["cppcheck_junit.py"],
     entry_points={
-        'console_scripts': [
-            'cppcheck_junit = cppcheck_junit:main',
+        "console_scripts": [
+            "cppcheck_junit = cppcheck_junit:main",
         ],
-    }
+    },
 )
