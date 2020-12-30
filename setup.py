20eec8ffe0e9e1650ce194608dfa25dff7bee0fa
johnthagen <johnthagen@gmail.com>
2020-12-30 13:12:59 -0500

    Switch to GitHub Actions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cppcheck-junit",
-    version="2.0.0",
+    version="2.1.0",
     description="Converts Cppcheck XML output to JUnit format.",
     long_description=open("README.rst").read(),
     keywords="Cppcheck C++ JUnit",
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules",
